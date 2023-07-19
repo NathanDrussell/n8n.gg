78c376ff5930ca3be41f3ce4a2c3753e5265e983
Nathan <nathan.d.russell@hotmail.com>
2023-07-19 15:02:00 -0400

    metaupdates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE =
+  "NathanRNFT | Web3 Full Stack Developer | Showcasing Proven Skills | Collaborative Expertise";
+export const SITE_DESCRIPTION = "Welcome to my website!";

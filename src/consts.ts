c328fc8ff236f56e56e8b05fc398a71338c15961
Nathan <nathan.d.russell@hotmail.com>
2023-07-19 15:04:06 -0400

    Descripotion

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,5 @@
 
 export const SITE_TITLE =
   "NathanRNFT | Web3 Full Stack Developer | Showcasing Proven Skills | Collaborative Expertise";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "I'm a passionate 25-year-old full stack developer with a strong background in web2 technologies. Explore my journey into web3 and collaborate with me to build exceptional products in the decentralized space. Let's embrace the exciting world of web3 together.";
